c02777680b1b5160243347fcedcdeae667bc86ad
Lee Chul <formathead79@gmail.com>
2020-01-15 18:23:57 +0900

    PlayQuiz
    1. Implememtation
      1) configureUI
      2) choiseBtnPressed
    2. Will DO
     1) out of range
     2) Implement the processbar

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -16,20 +16,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var choiceTwo: UIButton!
     @IBOutlet weak var choiceThree: UIButton!
     
-    
+    var questionNum = 0
+    var scoreNum = 0
+    var questionStr = QuizBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
     }
     
     
     @IBAction func choiseBtnPressed(_ sender: UIButton) {
-        
+        if questionNum + 1 <= questionStr.quiz.count {
+            let btnTitle = sender.titleLabel?.text
+            
+            if btnTitle == questionStr.quiz[questionNum].correctAnswer {
+                questionNum += 1
+                scoreNum += 1
+                configureUI()
+            } else {
+                questionNum += 1
+                configureUI()
+            }
+        } else {
+            questionNum = 0
+            scoreNum = 0
+        }
     }
     
-    
-    
-    
-    
+    func configureUI() {
+        questionTxt.text = questionStr.quiz[questionNum].q
+        scoreLbl.text = String(scoreNum)
+        
+        self.choiceOne.setTitle(questionStr.quiz[questionNum].a[0], for: .normal)
+        self.choiceTwo.setTitle(questionStr.quiz[questionNum].a[1], for: .normal)
+        self.choiceThree.setTitle(questionStr.quiz[questionNum].a[2], for: .normal)
+    }
     
 }//End Of The Class
